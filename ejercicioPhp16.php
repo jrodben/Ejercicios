deb09a281de7024a7f02532443b0f0038cddc272
jrodben <jrodben@g.educaand.es>
2023-10-24 02:47:30 +0200

    Ejercicio 16

---
+++ ejercicioPhp16.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comprobar y Formatear Texto</title>
+    <!-- Enlace a Bootstrap CSS a través de una CDN -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container mt-5">
+        <h1>Comprobar y Formatear Texto</h1>
+        <?php
+        $texto = "";
+
+        // Comprobamos si la variable está vacía
+        if (empty($texto)) {
+            $texto = "esto es un texto en minúsculas";
+            echo "<p><strong>" . strtoupper($texto) . "</strong></p>";
+        } else {
+            echo "<p>" . $texto . "</p>";
+        }
+        ?>
+    </div>
+</body>
+</html>
