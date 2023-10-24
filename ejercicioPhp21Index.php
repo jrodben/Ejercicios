7e4ac21d46aafaf726622e2621620bad1f3591e1
jrodben <jrodben@g.educaand.es>
2023-10-24 02:48:08 +0200

    Ejercicio 21

---
+++ ejercicioPhp21Index.php
@@ -0,0 +1,6 @@
+<?php include("ejercicioPhp21Header.php"); ?>
+<div class="container mt-5">
+    <h1>Página de ejemplo</h1>
+    <p>Esta página es de prueba para los includes.</p>
+</div>
+<?php include("ejercicioPhp21Footer.php"); ?>
