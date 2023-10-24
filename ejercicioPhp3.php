52925ccb07e00836513d6eca9f52c01b66488e72
jrodben <jrodben@g.educaand.es>
2023-10-24 02:04:34 +0200

    Ejercicio3

---
+++ ejercicioPhp3.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cuadrados de los 30 primeros números naturales</title>
+    <!-- Enlace a Bootstrap CSS a través de una CDN -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container mt-5">
+        <h1>Cuadrados de los 30 primeros números naturales</h1>
+        <ul class="list-group">
+            <?php
+            for ($i = 1; $i <= 30; $i++) {
+                $cuadrado = $i * $i;
+                $paridad = ($cuadrado % 2 == 0) ? 'Par' : 'Impar';
+                echo "<li class='list-group-item'>El cuadrado de $i es $cuadrado ({$paridad})</li>";
+            }
+            ?>
+        </ul>
+    </div>
+</body>
+</html>
